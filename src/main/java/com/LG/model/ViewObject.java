e53bd10ac9262d4ca54b9eea04219d36578929ff
liguo <b16020913@njupt.edu.cn>
2018-09-06 08:14:45 +0800

    • 评论中心 • 站内信

---
+++ src/main/java/com/LG/model/ViewObject.java
@@ -0,0 +1,18 @@
+package com.LG.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//vo用来传递对象和视图中间的一个对象；可以将所有东西通过set放进来，直接返回给前端；
+//主页广场上面东西的传递；
+public class ViewObject {
+    private Map <String, Object> objs = new HashMap <String, Object>();
+
+    public void set(String key, Object value) {
+        objs.put( key, value );
+    }
+
+    public Object get(String key) {
+        return objs.get( key );
+    }
+}
