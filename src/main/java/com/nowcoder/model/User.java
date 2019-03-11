e8342fc333169cac99f77aa0f8463962fdd17d3c
可以叫我利利 <2223686658@qq.com>
2019-03-11 19:59:10 +0800

    增加lombok的注解；删除模板代码

--- src/main/java/com/nowcoder/model/User.java
+++ src/main/java/com/nowcoder/model/User.java
@@ -1,8 +1,8 @@
 package com.nowcoder.model;
 
-/**
- * Created by nowcoder on 2016/6/26.
- */
+
+
+@Data
 public class User {
     private int id;
     private String name;
@@ -20,43 +20,5 @@ public class User {
         this.headUrl = "";
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getSalt() {
-        return salt;
-    }
-
-    public void setSalt(String salt) {
-        this.salt = salt;
-    }
-
-    public String getHeadUrl() {
-        return headUrl;
-    }
-
-    public void setHeadUrl(String headUrl) {
-        this.headUrl = headUrl;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 }
--- src/main/java/com/nowcoder/model/ViewObject.java
+++ src/main/java/com/nowcoder/model/ViewObject.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 //vo用来传递对象和视图中间的一个对象；可以将所有东西通过set放进来，直接返回给前端；
 //主页广场上面东西的传递；
+
 public class ViewObject {
     private Map <String, Object> objs = new HashMap <String, Object>();
 
